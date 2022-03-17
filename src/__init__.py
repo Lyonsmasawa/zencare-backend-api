7ba5632575c54e5c032e322d8f31b5a4d00e749a
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-03-17 22:58:21 +0300

    redeploy

--- src/__init__.py
+++ src/__init__.py
@@ -2,13 +2,23 @@ from flask import Flask, jsonify
 import os
 from src.auth import auth
 from src.database import db
-from config import config_options
 from flask_jwt_extended import JWTManager
 
-def create_app(config_name):
-    app = Flask(__name__)
+def create_app(test_config=None):
 
-    app.config.from_object(config_options[config_name])
+    app=Flask(__name__, instance_relative_config=True)
+
+    if test_config is None:
+
+        app.config.from_mapping(
+            SECRET_KEY=os.environ.get('SECRET_KEY'),
+            SQLALCHEMY_DATABASE_URI=os.environ.get("DATABASE_URL").replace('postgres://', 'postgresql://'),
+            SQLALCHEMY_TRACK_MODIFICATIONS=False,
+            JWT_SECRET_KEY=os.environ.get('JWT_SECRET_KEY')
+        )
+
+    else:
+        app.config.from_mapping(test_config)
 
     db.app=app
     db.init_app(app)
@@ -17,6 +27,5 @@ def create_app(config_name):
 
     app.register_blueprint(auth)
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL").replace('postgres://', 'postgresql://')
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     return app
