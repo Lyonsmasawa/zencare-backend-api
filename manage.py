8ff6918c1234d0d7b4f8a99bee2fccdb4f566501
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-03-17 21:55:28 +0300

    API

---
+++ manage.py
@@ -0,0 +1,14 @@
+from src import create_app, db
+from flask_script import Manager, Server
+from flask_migrate import Migrate, MigrateCommand
+
+app = create_app()
+
+manager = Manager(app)
+migrate = Migrate(app, db)
+
+manager.add_command('server', Server)
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+    manager.run()
